fe9d3ee165c9713501a840a28cc9ac79a9b69208
Mohamad <=>
2022-11-10 04:00:47 +0700

    menambahkan source code main,login,register,home

---
+++ lib/home.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+import 'package:sikost/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:math' as math;
+
+class home extends StatefulWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  logout() async {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Berhasil logout!')),
+    );
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.remove('username');
+    preferences.remove('password');
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => loginPage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size(MediaQuery.of(context).size.width, 170),
+          child: Column(
+            children: [
+              AppBar(
+                toolbarHeight: 140,
+                backgroundColor: Colors.white,
+                elevation: 0,
+                title: Column(
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CircleAvatar(
+                          radius: 38,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "Nama User",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              "Welcome Back, User!",
+                              style: TextStyle(
+                                  color: Colors.grey.shade700, fontSize: 16),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 30,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 35),
+                  child: Text(
+                    "Pengingat Untukmu",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        body: Container(
+          child: Stack(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color.fromARGB(255, 20, 136, 204),
+                    Color.fromARGB(255, 43, 50, 178),
+                  ],
+                )),
+              ),
+              Transform.rotate(
+                angle: -math.pi / 3.3,
+                alignment: Alignment.topLeft,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.shade600,
+                        spreadRadius: 0,
+                        blurRadius: 8,
+                        offset: Offset(4, 4))
+                  ]),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration:
+                    BoxDecoration(color: Color.fromARGB(0, 254, 254, 254)),
+                child: SingleChildScrollView(
+                    child: Center(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 20),
+                      ),
+                      Container(
+                        height: 200,
+                        width: 317,
+                        color: Colors.black,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        height: 200,
+                        width: 317,
+                        color: Colors.black,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        height: 200,
+                        width: 317,
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
+                )),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: new Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.home),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.insert_chart),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.group),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.warning_amber_rounded),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.person),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
